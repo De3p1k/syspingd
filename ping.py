c4a99275481546054786f6ebfb487508c122357a
Deepak <98681650+De3p1k@users.noreply.github.com>
2024-09-09 19:57:40 +0530

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,11 +1,22 @@
 import os
 import subprocess 
-#hostname = "google.com" #example
-response = os.system("ping -q -c 1 eepaserves > /dev/null")
+
+response = os.system("ping -q -c 1 <IP OR HOST NAME OF THE SERVER> > /dev/null")
 
 if response == 0:
-  #print (hostname, 'is up!')
-  os.system("dunstify 'The server is up!'")
+  file = open("<PATH TO YOUR LOG FILE>", 'r')
+  if file.readline() !='A':
+    file.close()
+    os.system("dunstify --icon <PATH TO YOUR ICON> 'The server is up!'")
+    file = open("<PATH TO YOUR LOG FILE>", 'w')
+    file.write('A')
+    file.close()
 else:
-  #print (hostname, 'is down!')
-  os.system("dunstify 'The server is down'")
+  file = open('<PATH TO YOUR LOG FILE>', 'r')
+  if file.readline() == 'A':
+    file.close()
+    os.system("dunstify --icon <PATH TO YOUR ICON> 'The server is down'")
+    file = open("<PATH TO YOUR LOG FILE>", 'w')
+    file.write('B')
+    file.close()
+file.close()%                                                                                                                                                                                                                                                         
